25e162fe3d58a63ffec5eadbf8a0a7b5d3f1ba07
splig <craigsg@gmail.com>
2017-02-06 20:45:32 +0000

    Added tests for and code for hot and cold service charges etc

--- src/sit/CafeX.scala
+++ src/sit/CafeX.scala
@@ -24,13 +24,54 @@ object CafeX extends App {
     pennies.toFloat / 100
   }
 
+  private def getItemIsFood(itemName: String): Boolean = {
+    val item = menuItems.get(itemName)
+    if (item.isEmpty) false else (item.get.foodOrDrink == Food)
+  }
+
+  private def getItemIsHotFood(itemName: String): Boolean = {
+    val item = menuItems.get(itemName)
+    if (item.isEmpty) false else (item.get.foodOrDrink == Food && item.get.hotOrCold == Hot)
+  }
+
+  // Functions of for calculating Service Charge 
+  private def hasFood(items: List[String]): Boolean = {
+    items.foldLeft(false)((hasSomeFood, itemName) => getItemIsFood(itemName) || hasSomeFood)
+  }
+
+  private def hasHotFood(items: List[String]): Boolean = {
+    items.foldLeft(false)((hasHotFood, itemName) => getItemIsHotFood(itemName) || hasHotFood)
+  }
+
+  private def applyServiceCharge(hasFood: Boolean, hasHotFood: Boolean, price: Int): Int = {
+    // Any Food - 10%
+    // Any Hot Food - 20%
+
+    def capService(x: Int) = if (x <= 2000) x else 2000
+
+    def calcService(x: Int, level: Float) = {
+      val service = ((x * 1.0f) * (level / 100)).toInt
+      capService(service)
+    }
+
+    def applyCorrectService(f: Boolean, h: Boolean, p: Int) = (f, h, p) match {
+      case (_, true, _) => p + calcService(p, 20.0f)
+      case (true, false, _) => p + calcService(p, 10.0f)
+      case _ => p
+    }
+
+    applyCorrectService(hasFood, hasHotFood, price)
+  }
+
   //********************************************************
   // Public Methods
 
-  def totalBillFloat(items: List[String]): Float = {
+  def totalBillFloat(items: List[String]): String = {
 
     val billInPennies = this.calculateTotalBill(items)
-    calculateFormattedBill(billInPennies)
+    val billWithService = applyServiceCharge(hasFood(items), hasHotFood(items), calculateTotalBill(items))
+    val total = calculateFormattedBill(billWithService)
+    "%1.2f".format(total)
   }
 
 }
--- src/sit/Tests.scala
+++ src/sit/Tests.scala
@@ -6,17 +6,27 @@ object Tests extends App {
   println(" Cola Price " + cola.price)
   assert(cola.price == 50)
 
+  // Adding *expensive* hot and cold food to test the service charge cap
   CafeX.setMenuItems(Map(
     "Cola" -> new MenuItem(50, Cold, Drink),
     "Coffee" -> new MenuItem(100, Hot, Drink),
     "Cheese Sandwich" -> new MenuItem(200, Cold, Food),
-    "Steak Sandwich" -> new MenuItem(450, Hot, Food)))
+    "Steak Sandwich" -> new MenuItem(450, Hot, Food),
+    "Eels" -> new MenuItem(11000, Hot, Food),
+    "Cabbage" -> new MenuItem(11000, Cold, Food)))
 
+    
   val total = CafeX.totalBillFloat(List("Cola", "Coffee", "Cheese Sandwich"))
   println(" Total " + total)
-  assert(total == 3.5f)
+  assert(total == "3.85")
     
+  val cabbage = CafeX.totalBillFloat(List("Cabbage"))
+  println(" cabbage " + cabbage)
+  assert(cabbage == "121.00")	// 10% Service Charge for Cold Food 
   
+  val eels = CafeX.totalBillFloat(List("Eels"))
+  println(" eels " + eels)
+  assert(eels == "130.00")	// Max £20 Service Charge for Hot Food 
   
   println("All Tests Passed (if you can read this!)" )  
 }
